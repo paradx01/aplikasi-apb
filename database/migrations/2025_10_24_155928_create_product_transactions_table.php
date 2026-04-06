31ab02da1ce6d9880ba965d6e44ad1d2deb10c46
paradx01 <dannytoharuddinali@gmail.com>
2026-04-06 12:34:14 +0700

    edit migration

--- database/migrations/2025_10_24_155928_create_product_transactions_table.php
+++ database/migrations/2025_10_24_155928_create_product_transactions_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('product_transactions', function (Blueprint $table) {
+        Schema::create('product_transactions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');    
             $table->unsignedBigInteger('total_amount');
@@ -43,16 +43,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('product_transactions', function (Blueprint $table) {
-            $table->dropForeign(['user_address_id']);
-            $table->dropColumn([
-                'address', 
-                'city', 
-                'post_code', 
-                'phone_number', 
-                'notes',
-                'user_address_id'
-            ]);
-        });
+        Schema::dropIfExists('product_transactions');
     }
 };
